369bae589e6bfa542abb17e467d7bbe48ab5d770
Francis Aguinaldo <fryaguinaldo@gmail.com>
2022-05-10 22:22:56 -1000

    added .json files

--- Develop/schema.sql
+++ Develop/schema.sql
@@ -1,7 +1,7 @@
-DROP DATABASE IF EXISTS employee-tracker_db;
-CREATE DATABASE employee-tracker_db;
+DROP DATABASE IF EXISTS employee_tracker_db;
+CREATE DATABASE employee_tracker_db;
 
-USE employee-tracker_db;
+USE employee_tracker_db;
 
 CREATE TABLE department (
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
