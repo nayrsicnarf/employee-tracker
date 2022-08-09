efc64b1348d0ce1279c039fac1c41cb680bb3378
Francis Aguinaldo <fryaguinaldo@gmail.com>
2022-08-09 11:13:09 -0700

    updated db

--- db/schema.sql
+++ db/schema.sql
@@ -4,21 +4,30 @@ CREATE DATABASE employee_tracker_db;
 USE employee_tracker_db;
 
 CREATE TABLE department (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30) -- to hold department name
 );
 
-CREATE TABLE role (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    title VARCHAR(30), -- to hold role title
-    salary DECIMAL, -- to hold salary
+CREATE TABLE employeeRole (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL, -- to hold role title
+    salary DECIMAL NOT NULL, -- to hold salary
     department_id INT NOT NULL -- to hold reference to department role belongs to
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    first_name VARCHAR(30), -- to hold employee first name
-    last_name VARCHAR(30), -- to hold employee last name
-    role_id INT NOT NULL, -- to hold reference to employee role
+    first_name VARCHAR(30) NOT NULL, -- to hold employee first name
+    last_name VARCHAR(30) NOT NULL, -- to hold employee last name
+    role_id INT, -- to hold reference to employee role
     manager_id INT NULL -- to hold reference to another employee that is the manager of the current employee (null if the employee has no manager)
+    FOREIGN KEY (role_id)
+    REFERENCES empRole(id)
+    ON DELETE SET NULL,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
 );
